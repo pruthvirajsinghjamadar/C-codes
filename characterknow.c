c485bf76a68ab5b8ecf4714e86d0f7d3fc6b4b55
pruthvirajsinghjamadar <pruthvirajsinghjamadar@gmail.com>
2025-11-05 02:07:03 +0530

    Add files via upload

---
+++ characterknow.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main () {
+char ch;
+printf("enter the ch");
+scanf("%d",ch);
+if((ch>=a&&ch<=z)||(ch>=A&&ch<=z)){
+    printf("character is is alphabet");
+}else if(ch<=0&&ch>=9){
+    printf("character is digit");
+}else   {
+    printf("entered ch special")
+}}
